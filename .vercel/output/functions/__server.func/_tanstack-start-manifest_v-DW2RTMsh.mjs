a3ac9b1ccb278e0c50e9f3c3eb816e55772b428f
arashmakkyy <arashmakkywork@gmail.com>
2026-09-09 12:10:55 -0400

    Initial commit

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-DW2RTMsh.mjs
@@ -0,0 +1,29 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-DW2RTMsh.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/workspace/src/routes/__root.tsx",
+		children: ["/", "/g/$groupId"],
+		preloads: [
+			"/assets/index-iOLiExy_.js",
+			"/assets/rolldown-runtime-CbXtAM7H.js",
+			"/assets/preload-helper-Cz6P6_9o.js"
+		],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-iOLiExy_.js"
+		} }]
+	},
+	"/": {
+		filePath: "/workspace/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-6g3AzNuH.js", "/assets/share-ey5Cx7Hn.js"]
+	},
+	"/g/$groupId": {
+		filePath: "/workspace/src/routes/g.$groupId.tsx",
+		children: void 0,
+		preloads: ["/assets/g._groupId-C1c4c7Hu.js", "/assets/share-ey5Cx7Hn.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
